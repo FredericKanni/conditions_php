d2d4f2b000c30f6977a0a0cacaade9314ce3787c
frederic <kannif@ymail.com>
2019-09-30 14:05:33 +0400

    exo7

---
+++ exo7/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<?php
+    $isOk =true;
+   
+    
+    if(!$isOk)
+    {
+        echo'c\'est pas bon !!!';
+    }
+    else
+    {
+        echo 'c\'est ok !!';
+    }
+
+?>
+
+ 
+</body>
+</html>
