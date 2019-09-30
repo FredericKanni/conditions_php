6b93991cf089ead012bbdbe3f9ed9b40e117bb4b
frederic <kannif@ymail.com>
2019-09-30 12:58:55 +0400

    exo5

---
+++ exo5/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<?php
+    $gender ="Homme";
+   
+    
+    if($gender != "Homme")
+    {
+        echo'C\'est une développeuse !!!';
+    }
+    else
+    {
+        echo'C\'est un développeur !!';
+    }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+ 
+</body>
+</html>
